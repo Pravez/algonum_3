150dae5a9751a416731865485f193d64aa6d65c9
Pravez <paul.ravezb@gmail.com>
2016-03-04 12:03:43 +0100

    Application débile pour l'instant de householder

--- Transformation_Householder.py
+++ Transformation_Householder.py
@@ -1,14 +1,25 @@
 import numpy as np
 
 
-def findUVector(x, y):
+def findVector(x, y):
     num = x-y
     denom = np.linalg.norm((x-y))
     return num/denom
 
 def householderProjection(x, y):
-    u = findUVector(x, y)
+    u = findVector(x, y)
     n = len(x)
     return np.identity(n)-2*(np.dot(u, np.transpose(u)))
 
-print(householderProjection(np.matrix('[3; 4; 0.]'), np.matrix('[0; 0; 5.]')))
+def applyHouseholderVector(x, y, u):
+    n = findVector(x, y)
+    return (u - 2*(np.dot(n, np.transpose(n)))*u)
+
+def applyDumbHouseholderMatrix(m, h):
+    return np.dot(m, h)
+
+def applyOptimalHouseholderVector(x, y, u):
+    n = findVector(x, y)
+    return
+
+print(applyHouseholderVector(np.matrix('[3; 4; 0.]'), np.matrix('[0; 0; 5.]'), np.matrix('[3; 4; 0.]')))
