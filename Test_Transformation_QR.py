bde938fc4342e85bfab0eec33485c98d311ebe13
Pravez <paul.ravezb@gmail.com>
2016-03-11 12:27:14 +0100

    Tests sur QR

--- Test_Transformation_QR.py
+++ Test_Transformation_QR.py
@@ -1,3 +1,42 @@
 import numpy as np
+import unittest
 from Transformations_QR import *
+import matplotlib.pyplot as plt
 
+class TestQRTransform(unittest.TestCase):
+
+    def QR_Converge(self):
+        np.set_printoptions(1, suppress=True)
+        B = np.mat(100*np.random.rand(10, 10))
+        QLeft, BD, QRight = getBidiagonal(B)
+
+        iter = [i for i in range(1, 50)]
+        conv = [0 for i in range(1, 50)]
+
+        for i in range(1, 50):
+            U, S, V = qr_transform(BD, i)
+            assert(np.all(abs(U*S*V - BD) < epsilon))
+
+            for j in range(0, 10):
+                for k in range(0, 10):
+                    if(j<>k and abs(S[j, k])>epsilon):
+                        conv[i]+=1
+        plt.plot(iter, conv)
+        plt.show()
+
+np.set_printoptions(1, suppress=True)
+B = np.mat(100*np.random.rand(10, 10))
+QLeft, BD, QRight = getBidiagonal(B)
+
+iter = [i for i in range(1, 50)]
+conv = [0 for i in range(1, 50)]
+
+for i in range(1, 50):
+    U, S, V = qr_transform(BD, i)
+
+    for j in range(0, 10):
+        for k in range(0, 10):
+            if(j<>k and abs(S[j, k])>epsilon):
+                conv[i]+=1
+plt.plot(iter, conv)
+plt.show()
--- Transformations_QR.py
+++ Transformations_QR.py
@@ -1,13 +1,14 @@
 import numpy as np
 from Forme_Bidiagonale import *
+import matplotlib.pyplot as plt
 
 
-def qr_transform(BD):
+def qr_transform(BD, NMax):
     n, m = BD.shape
     U = np.mat(np.identity(n))
     V = np.mat(np.identity(m))
     S = BD
-    for i in range(0, 1000):
+    for i in range(0, NMax):
         (Q1, R1) = np.linalg.qr(np.transpose(S))
         (Q2, R2) = np.linalg.qr(np.transpose(R1))
         S = R2
@@ -16,7 +17,19 @@ def qr_transform(BD):
     return (U, S, V)
 
 
+np.set_printoptions(1, suppress=True)
+B = np.mat(100*np.random.rand(10, 10))
+QLeft, BD, QRight = getBidiagonal(B)
 
-np.set_printoptions(5)
-U, S, V = qr_transform(BD)
-print(np.all(abs(U*S*V-BD)<10**(-10)))
+iter = [i for i in range(1, 50)]
+conv = [0 for i in range(1, 50)]
+
+for i in range(1, 50):
+    U, S, V = qr_transform(BD, i)
+
+    for j in range(0, 10):
+        for k in range(0, 10):
+            if(j<>k and abs(S[j, k])>epsilon):
+                conv[i-1]+=1
+plt.plot(iter, conv)
+plt.show()
