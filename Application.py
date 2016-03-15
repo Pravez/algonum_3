0c33c100db761323d0f658432a617d49847d05bf
Pravez <paul.ravezb@gmail.com>
2016-03-15 16:51:39 +0100

    Réorganisation

--- Application.py
+++ Application.py
@@ -1,30 +1,19 @@
-import matplotlib.image as mimg
-import matplotlib.pyplot as plt
 import matplotlib
-import numpy as np
-import pylab
 from scipy import misc
 
 from Transformations_QR import *
-from Transformation_Householder import *
-from Forme_Bidiagonale import *
-
-
-def average(pixel):
-    return (pixel[0]*0.299 + pixel[1]*0.587 + pixel[2]*0.114)/3
 
 
 
-def remove_on_diag(S, k):
+def compress_rank_k(S, k):
     n, m = S.shape
-    for i in range(k, min(n, m)):
+    for i in range(k, min(n, m)-1):
         S[i, i] = 0
 
     return S
 
-
 #define region
-def getPixels(image, pix):
+def get_pixels_from_color(image, pix):
     n, m, s = image.shape
     ret = np.mat(np.zeros((n, m)))
     for i in range(0, n):
@@ -33,64 +22,47 @@ def getPixels(image, pix):
 
     return ret
 
+def normalize_pixel(pix):
+    if(pix[0] > 255):
+        pix[0] = 255
+    if(pix[1] > 255):
+        pix[1] = 255
+    if(pix[2] > 255):
+        pix[2] = 255
+    return pix
+
+def apply_compression(matrix, compression_rate, qr_rate):
+    QL, BD, QR = getBidiagonal(matrix)
+    U, S, V = qr_transform(BD, qr_rate)
+    S = compress_rank_k(S, compression_rate)
+
+    return QL*U*S*V*QR
+
 def reconstitute_image(red, green, blue):
     n, m = red.shape
     res = np.zeros((n, m, 3), dtype="uint8")
     for i in range(0, n):
         for j in range(0, m):
-            if(red[i, j] > 255):
-                red[i, j] = 255
-            if(green[i, j] > 255):
-                green[i, j] = 255
-            if(blue[i, j] > 255):
-                blue[i, j] = 255
-            res[i][j][0] = red[i, j]
-            res[i][j][1] = green[i, j]
-            res[i][j][2] = blue[i, j]
+            res[i][j] = normalize_pixel([red[i, j], green[i, j], blue[i, j]])
 
     return res
 
-def apply_compression(matrix):
-    QL, BD, QR = getBidiagonal(matrix)
-    U, S, V = qr_transform(BD, 20)
-    S = remove_on_diag(S, 30)
-
-    return QL*U*S*V*QR
-
-def Apply_to_image(image):
+def Compress_Image(image):
     img = misc.imread(image)
+    qr_rate = 20
+    compression_rate = 30
 
-    red = getPixels(img, 0)
-    green = getPixels(img, 1)
-    blue = getPixels(img, 2)
+    red = get_pixels_from_color(img, 0)
+    green = get_pixels_from_color(img, 1)
+    blue = get_pixels_from_color(img, 2)
 
-    red_c = apply_compression(red)
-    green_c = apply_compression(green)
-    blue_c = apply_compression(blue)
+    red_c = apply_compression(red, compression_rate, qr_rate)
+    green_c = apply_compression(green, compression_rate, qr_rate)
+    blue_c = apply_compression(blue, compression_rate, qr_rate)
 
-    rec = reconstitute_image(red_c, green_c, blue_c)
-    #print(rec)
+    image_compressed = reconstitute_image(red_c, green_c, blue_c)
 
-    plt.imshow(rec)
+    plt.imshow(image_compressed)
     plt.show()
 
-
-def Apply_to_image_with_gs(image):
-    img = misc.imread(image)
-
-    grey = np.zeros((img.shape[0], img.shape[1])) # init 2D numpy array
-    # get row number
-    for rownum in range(len(img)):
-       for colnum in range(len(img[rownum])):
-          grey[rownum][colnum] = average(img[rownum][colnum])
-
-    grey = np.matrix(grey)
-    QL, BD, QR = getBidiagonal(grey)
-    U, S, V = qr_transform(BD, 20)
-    S = remove_on_diag(S, 10)
-
-    rec = QL*U*S*V*QR
-    plt.imshow(rec, cmap = matplotlib.cm.Greys_r)
-    plt.show()
-
-Apply_to_image("p3_takeoff_base.png")
+Compress_Image("p3_takeoff_base.png")
